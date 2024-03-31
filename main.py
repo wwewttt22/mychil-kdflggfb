84fd43e79ab6ac10494049a825d90566009960d5
ogurets <cxrsed.soul681@gmail.com>
2024-03-31 15:19:33 +0300

    set window

--- main.py
+++ main.py
@@ -1,15 +1,35 @@
 from pygame import*
-
-
-window_size = (300,300)
-
+from gamesprite import *
 
 
 
+WINDOWSIZE = (800,600)
+
+TXTCOLOR = (134,74,249)
+BGCOLOR = (248,229,89)
+
+window = display.set_mode(WINDOWSIZE)
+display.set_caption('pinpon')
+
+timer = time.Clock()
+run = True
+
+
+
+
+
+while run:
+    for e in event.get():
+        if e.type == QUIT:
+            run = False
+    window.fill(BGCOLOR)    
+    display.update()
+    timer.tick(60)
 
 
 
 
 
 
-while True
+
+
